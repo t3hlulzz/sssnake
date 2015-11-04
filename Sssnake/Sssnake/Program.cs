9efd822a96acc36e97044ab759d79b0c07a1139c
t3hlulzz <t3hlulzz@gmail.com>
2015-11-04 17:39:37 +0200

    lists

--- Sssnake/Sssnake/Program.cs
+++ Sssnake/Sssnake/Program.cs
@@ -17,7 +17,38 @@ namespace Sssnake
             Point p2 = new Point(4, 5, '#');
                       p2.Draw();
 
-          
+            List<int> numlist = new List<int>();
+            numlist.Add(0);
+            numlist.Add(1);
+            numlist.Add(2);
+            numlist.Add(3);
+            numlist.Add(4);
+
+            int x = numlist[0];
+            int y = numlist[1];
+            int z = numlist[2];
+
+            foreach(int i in numlist)
+            {
+                Console.WriteLine(i);
+            }
+
+            /*numlist.RemoveAt(0);
+
+            List<Point> plist = new List<Point>();
+            plist.Add(p1);
+            plist.Add(p2);*/
+
+            Console.WriteLine("=====");
+
+            List<char> charlist = new List<char>();
+            charlist.Add('#');
+            charlist.Add('*');
+
+            foreach(char i in numlist)
+            {
+                Console.WriteLine(i);
+            }
 
             Console.ReadLine();
         }
