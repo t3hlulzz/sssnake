b1cd2e52f30c64369a04bdb628da13c671817e9c
t3hlulzz <t3hlulzz@gmail.com>
2015-11-04 15:34:07 +0200

    constructors

--- Sssnake/Sssnake/Program.cs
+++ Sssnake/Sssnake/Program.cs
@@ -11,17 +11,11 @@ namespace Sssnake
         static void Main(string[] args)
         {
 
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 2;
-            p1.sym = '*';
-            p1.Draw();
+            Point p1 = new Point(1, 3, '*');
+                       p1.Draw();
 
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
-            p2.Draw();
+            Point p2 = new Point(4, 5, '#');
+                      p2.Draw();
 
           
 
