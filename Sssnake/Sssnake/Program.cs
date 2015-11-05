cbdfba0154be5aa6af92927d1443acb19df23272
t3hlulzz <t3hlulzz@gmail.com>
2015-11-05 15:41:02 +0200

    vline

--- Sssnake/Sssnake/Program.cs
+++ Sssnake/Sssnake/Program.cs
@@ -17,38 +17,11 @@ namespace Sssnake
             Point p2 = new Point(4, 5, '#');
                       p2.Draw();
 
-            List<int> numlist = new List<int>();
-            numlist.Add(0);
-            numlist.Add(1);
-            numlist.Add(2);
-            numlist.Add(3);
-            numlist.Add(4);
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Drw();
 
-            int x = numlist[0];
-            int y = numlist[1];
-            int z = numlist[2];
-
-            foreach(int i in numlist)
-            {
-                Console.WriteLine(i);
-            }
-
-            /*numlist.RemoveAt(0);
-
-            List<Point> plist = new List<Point>();
-            plist.Add(p1);
-            plist.Add(p2);*/
-
-            Console.WriteLine("=====");
-
-            List<char> charlist = new List<char>();
-            charlist.Add('#');
-            charlist.Add('*');
-
-            foreach(char i in numlist)
-            {
-                Console.WriteLine(i);
-            }
+            VerticalLine vline = new VerticalLine(10, 4, 8, '-');
+            vline.Drw2();
 
             Console.ReadLine();
         }
---
+++ Sssnake/Sssnake/VerticalLine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sssnake
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int x, int yTop, int yBot, char sym)
+        {
+            pList = new List<Point>();
+
+            for (int y = yTop; y < yBot; y++)
+
+            {
+                Point p2 = new Point(x, y, sym);
+
+                pList.Add(p2);
+            }
+        }
+
+            public void Drw2()
+        {
+            foreach (Point p2 in pList)
+            {
+                p2.Draw();
+            }
+        }
+    }
+}
