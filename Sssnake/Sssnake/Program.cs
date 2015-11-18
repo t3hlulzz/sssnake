c7744cd530a9fc27d00e44788f65058c9c89bd16
t3hlulzz <t3hlulzz@gmail.com>
2015-11-18 11:53:57 +0200

    Keymovecommand

--- Sssnake/Sssnake/Program.cs
+++ Sssnake/Sssnake/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Sssnake
@@ -22,44 +23,21 @@ namespace Sssnake
             leftLine.Drw();
             rightLine.Drw();
 
-            Point p = new Point(20, 12, '>');
+            Point p = new Point(20, 12, '$');
             Snake snake = new Snake( p, 4, Direction.RIGHT);
             snake.Drw();
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
-            System.Threading.Thread.Sleep(300);
-            snake.Move();
 
-            Console.Write("G");
-            System.Threading.Thread.Sleep(200);
-            Console.Write("a");
-            System.Threading.Thread.Sleep(200);
-            Console.Write("m");
-            System.Threading.Thread.Sleep(200);
-            Console.Write("e");
-            System.Threading.Thread.Sleep(200);
-            Console.Write("O");
-            System.Threading.Thread.Sleep(200);
-            Console.Write("v");
-            System.Threading.Thread.Sleep(200);
-            Console.Write("e");
-            System.Threading.Thread.Sleep(200);
-            Console.Write("r");
-            System.Threading.Thread.Sleep(200);
-            Console.ReadLine();
+            while(true)
+            {
+                if(Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+
+                Thread.Sleep(100);
+                snake.Move();
+            }
         }
         
 
